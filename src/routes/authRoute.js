de63acacc5e29288f2ed58372ae17cacb14d7097
Felix Nguyen <phucnguyenngo2005@gmail.com>
2026-02-13 09:36:20 +0700

    Delete src/routes directory

--- src/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { login, register } from '../controller/authController.js';
-
-const route = express.Router();
-
-route.post('/', login);
-route.post('/register', register);
-
-export default route;
